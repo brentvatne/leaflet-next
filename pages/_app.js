51583d1390e2e880ad517a5682636b3cd1f43501
Brent Vatne <brentvatne@gmail.com>
2020-10-05 23:05:42 -0700

    example

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import 'leaflet/dist/leaflet.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
 export default MyApp
